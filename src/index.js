fc2163a6a138c5019737b951e4a8753a19bb6bba
Jack Fetkovich <johnfetkovich1014@gmail.com>
2021-05-08 01:00:41 -0400

    main app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,144 @@
-import React from 'react';
+import { react, useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import style from '../src/Style.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Calculator() {
+	const [expression, setExpression] = useState('');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	return (
+		<div className="calculator" >
+			<Screen expression={expression}></Screen>
+			<div className='buttonRow'>
+				<CalcButton
+					value={1}
+					setExpression={setExpression}
+					expression={expression}
+				/>
+				<CalcButton
+					value={2}
+					setExpression={setExpression}
+					expression={expression}
+				/>
+				<CalcButton
+					value={3}
+					setExpression={setExpression}
+					expression={expression}
+				/>
+				<CalcButton
+					value={'+'}
+					setExpression={setExpression}
+					expression={expression}
+				/>
+			</div>
+			<div className='buttonRow'>
+				<CalcButton
+					value={4}
+					setExpression={setExpression}
+					expression={expression}
+				/>
+				<CalcButton
+					value={5}
+					setExpression={setExpression}
+					expression={expression}
+				/>
+				<CalcButton
+					value={6}
+					setExpression={setExpression}
+					expression={expression}
+				/>
+				<CalcButton
+					value={'-'}
+					setExpression={setExpression}
+					expression={expression}
+				/>
+			</div>
+			<div className='buttonRow'>
+				<CalcButton
+					value={7}
+					setExpression={setExpression}
+					expression={expression}
+				/>
+				<CalcButton
+					value={8}
+					setExpression={setExpression}
+					expression={expression}
+				/>
+				<CalcButton
+					value={9}
+					setExpression={setExpression}
+					expression={expression}
+				/>
+				<CalcButton
+					value={'*'}
+					setExpression={setExpression}
+					expression={expression}
+				/>
+			</div>
+			<div className='buttonRow'>
+				<CalcButton
+					value={'.'}
+					setExpression={setExpression}
+					expression={expression}
+				/>
+				<CalcButton
+					value={0}
+					setExpression={setExpression}
+					expression={expression}
+				/>
+				<button
+					className="calcButton"
+					onClick={() => setExpression(eval(expression))}
+				>
+					=
+				</button>
+				<CalcButton
+					value={'/'}
+					setExpression={setExpression}
+					expression={expression}
+				/>
+			</div>
+			<div>
+				<button className="calcButton" onClick={() => setExpression('')}>
+					Clear
+				</button>
+			</div>
+		</div>
+	);
+}
+
+function CalcButton({ value, setExpression, expression }) {
+	const addValueToExpression = () => {
+		console.log(typeof expression);
+		setExpression(expression + value);
+	};
+
+
+	return (
+		<button
+			className="calcButton"
+			onClick={() => {
+				addValueToExpression();
+			}}
+		>
+			{value}
+		</button>
+	);
+}
+
+function Screen({ expression }) {
+	return (
+		<div className="screen">
+			<p>{expression}</p>
+		</div>
+	);
+}
+
+function App() {
+	return (
+		<div className="container">
+			<Calculator />
+		</div>
+	);
+}
+
+ReactDOM.render(<App style={style} />, document.getElementById('root'));
